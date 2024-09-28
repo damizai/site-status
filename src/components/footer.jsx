ce9e491e565108a3313dfce3af1ae8b11073e1fd
东方月初 <165906706+damizai@users.noreply.github.com>
2024-09-28 18:03:00 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <p>
           <CustomLink
             text={Package.alia}
-            to="https://github.com/imsyy/site-status"
+            to="https://github.com/damizai/site-status"
           />
           &nbsp;Version&nbsp;{Package.version}
         </p>
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://hexo.200038.xyz/" text="东方月初" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
